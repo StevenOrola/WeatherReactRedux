ed74c0affdfd157bc52e0ff53ecb0f371f222f21
Steven Orola <stevenorola@gmail.com>
2018-05-14 10:28:41 -0700

    added axios dependency to help with ajax requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 const API_KEY = 'dd1fb385cb100a1fd634dfbf38078014';
+const ROOT_URL = 'http://samples.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = ROOT_URL + '&q=' + city + ',us';
+	
+
+	return {
+		type: FETCH_WEATHER
+	};
+}
